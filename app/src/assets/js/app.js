9ba82977b75a288a14058b0fb70aeaceb3661d09
Rombt <Rombt@gmail.com>
2024-01-09 15:35:40 +0200

    gumburger menu start

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,4 +1,5 @@
 import { isWebp } from "./modules/isWebp.js"; // проверяет поддерживает ли браузер формат webp
+import { burgermenu } from "./modules/Burgermenu.js"; // проверяет поддерживает ли браузер формат webp
 
 
 
